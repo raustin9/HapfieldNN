4fb8ea04143e231f82741c774ce65fe2fe94cc93
raustin9 <raustin9@vols.utk.edu>
2024-03-07 23:06:48 -0500

    parse command line flags

--- network/src/network.cc
+++ network/src/network.cc
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 #include <cstdlib>
 #include <vector>
@@ -6,15 +7,37 @@
 #include "core/utils.h"
 #include "hopfield/hopfield.h"
 
-int main() {
-    constexpr u32 P = 50; // Number of vectors we want to encode
-    hopfield::hopfield hf = hopfield::hopfield::create_new(100); // create 100x100 hfnn
+int main(i32 argc, char** argv) {
+    i64 P = -1;
+    i64 neuron_count = -1;
+    i64 num_experiments = -1;
+    for (i32 i = 0; i < argc; i++) {
+        if (std::strcmp(argv[i], "-P") == 0) {
+            // Number of patterns to encode 
+            i++;
+            P = std::atoi(argv[i]);
+            std::printf("P:  %ld\n", P);
+        } else if (std::strcmp(argv[i], "-N") == 0) {
+            // Number of neurons for the networ 
+            i++;
+            neuron_count = std::atoi(argv[i]);
+            std::printf("NC: %ld\n", neuron_count);
+        } else if (std::strcmp(argv[i], "--num-experiments") == 0) {
+            // Number of experiments we want to run
+            i++;
+            num_experiments = std::atoi(argv[i]);
+            std::printf("NE: %ld\n", num_experiments);
+        } 
+    }
+
+
+    hopfield::hopfield hf = hopfield::hopfield::create_new(neuron_count); // create 100x100 hfnn
     std::vector<std::vector<i32> > patterns; // the 50 bipolar vectors to imprint
 
     // Create 50 random vectors of length 100 of bipolar {-1,1} values
     for (std::size_t x = 0; x < P; x++) {
         std::vector<i32> v;
-        for (std::size_t i = 0; i < 100; i++) {
+        for (std::size_t i = 0; i < neuron_count; i++) {
             v.push_back(core::rng());
         }
         patterns.push_back(v);
